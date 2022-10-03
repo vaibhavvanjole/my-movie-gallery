0b54ad4d1d867954fd6d3378714cdad1e87b601b
Vaibhav Vanjole <vaibhavvanjole07@gmail.com>
2022-10-03 08:44:25 +0530

    created backdrop, modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
 
 var cl = console.log;
 
+const showModalBtn = document.getElementById('showModalBtn');
+const backdrop = document.getElementById('backdrop')
+const myModal = document.getElementById('myModal')
+const myClose = [...document.querySelectorAll('.myClose')]
+
+const showModalHandler =(e) =>{
+    backdrop.classList.toggle('d-none');
+    myModal.classList.toggle('d-none');
+}
+
+
+
+
+showModalBtn.addEventListener('click', showModalHandler)
+myClose.forEach(arr=>{
+    arr.addEventListener('click', showModalHandler)
+})
