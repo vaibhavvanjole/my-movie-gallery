789b94fa0b1d9c967e217ac6a93c23cb9bd1c5ba
Vaibhav Vanjole <vaibhavvanjole07@gmail.com>
2022-10-03 09:16:49 +0530

    added movie card

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,16 +5,60 @@ const showModalBtn = document.getElementById('showModalBtn');
 const backdrop = document.getElementById('backdrop')
 const myModal = document.getElementById('myModal')
 const myClose = [...document.querySelectorAll('.myClose')]
+const titleControl = document.getElementById('title');
+const imgUrlControl = document.getElementById('imgUrl');
+const ratingControl = document.getElementById('rating')
+const movieForm = document.getElementById('movieForm')
+const movieContainer = document.getElementById('movieContainer')
 
-const showModalHandler =(e) =>{
+let movieArray = [];
+
+const creatMovie = (arr) =>{
+    let result = '';
+    arr.forEach(everyMovie =>{
+     result += `
+     <div class="col-md-4">
+                 <div class="card">
+                     <div class="card-boy">
+                         <h3>${everyMovie.title}</h3>
+                         <figure>
+                             <img src="${everyMovie.imgURL}" alt="${everyMovie.title}" title="${everyMovie.title}">
+                             <figcaption>
+                                 <p>${everyMovie.rating}/5</p>
+                             </figcaption>
+                         </figure>
+                     </div>
+                 </div>
+             </div>`
+    })
+    movieContainer.innerHTML = result;
+ }
+ 
+const modalShowHideHandler =(e) =>{
     backdrop.classList.toggle('d-none');
     myModal.classList.toggle('d-none');
 }
 
+const onMovieFormHandler=(e)=>{
+    e.preventDefault();
+    cl(e.target);
+
+    let obj ={
+        title:titleControl.value,
+        imgURL:imgUrlControl.value,
+        rating:ratingControl.value
+    }
+    movieArray.push(obj);
+    cl(movieArray)
+    creatMovie(movieArray);
+    e.target.reset();
+    modalShowHideHandler();
+}
 
 
-
-showModalBtn.addEventListener('click', showModalHandler)
+showModalBtn.addEventListener('click', modalShowHideHandler)
 myClose.forEach(arr=>{
-    arr.addEventListener('click', showModalHandler)
+    arr.addEventListener('click', modalShowHideHandler)
 })
+backdrop.addEventListener('click', modalShowHideHandler)
+movieForm.addEventListener('submit', onMovieFormHandler)
